9507f059e31cfa4b26fa9747a77a77aaaa524070
Ivanstwan <ivan.stwan@gmail.com>
2023-09-25 21:33:06 +0700

    [client] install prettier, adjust eslint prettier config

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -5,16 +5,20 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-hooks', 'tailwindcss', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'tailwindcss/classnames-order': 'warn',
+    'tailwindcss/no-custom-classname': 'warn',
+    'tailwindcss/no-contradicting-classname': 'error',
   },
-}
+};
--- client/README.md
+++ client/README.md
@@ -3,3 +3,7 @@ initial command
 1. npm create vite@latest client -> 'React' framework -> 'JavaScript' variant
 2. npm install -D tailwindcss postcss autoprefixer
 3. npx tailwindcss init -p
+4. npm i -D prettier eslint-config-prettier eslint-plugin-prettier
+   eslint-config-prettier - disabled rule that conflict with prettier
+   eslint-plugin-prettier - all prettier rule work in joncunction with eslint rule
+5. npm install -D prettier-plugin-tailwindcss (for sorting tailwind class)
