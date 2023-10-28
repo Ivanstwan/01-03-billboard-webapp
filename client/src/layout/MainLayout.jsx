976b1b0cfecee8ffa2bc049bd803fb6557c03cfc
Ivanstwan <ivan.stwan@gmail.com>
2023-10-28 22:27:01 +0700

    [client] install leaflet, adjust main layout

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,11 +1,11 @@
 export const MainLayout = ({ children }) => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-gray-200">
       {/* can use sidebar below */}
       {/* <MobileSidebar /> */}
       {/* <Sidebar /> */}
       <div className="flex min-h-full w-0 flex-1 flex-col">
-        <div className="bg-opacity-15 fixed top-0 z-10 flex h-16 w-full flex-shrink-0 bg-white p-6 shadow-md">
+        <div className="bg-opacity-15 fixed top-0 z-[1001] flex h-16 w-full flex-shrink-0 bg-white p-6 shadow-md">
           <button className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden">
             {/* <MenuIcon className="h-6 w-6" aria-hidden="true" /> */}
             Possible Hamburger OR open menu
@@ -19,9 +19,12 @@ export const MainLayout = ({ children }) => {
           </div>
         </div>
         <section className="flex flex-1 flex-col">
-          <main className="mt-16 grid grow grid-cols-[minmax(269px,1fr)_minmax(500px,4fr)] grid-rows-1 gap-8">
-            <nav className="w-60 bg-white shadow-md">Possible Sidebar</nav>
-            <section className="pr-8 pt-8">
+          {/* <main className="relative mt-16 grid grow grid-cols-[minmax(269px,1fr)_minmax(500px,4fr)] grid-rows-1 gap-8"> */}
+          <main className="relative mt-16 grid grow grid-cols-[minmax(269px,1fr)_minmax(500px,4fr)] grid-rows-1 gap-8">
+            <nav className="fixed left-0 top-[4rem] h-full w-60 bg-white shadow-md">
+              Possible Sidebar
+            </nav>
+            <section className="col-start-2 pr-8 pt-8">
               <article className="relative flex-1 overflow-y-auto rounded-[2px] bg-white p-10 shadow-md">
                 {children}
               </article>
