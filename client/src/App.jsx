5d44ae869fd4a114c9890d71285553416b130136
Ivanstwan <ivan.stwan@gmail.com>
2023-12-07 22:46:35 +0700

    [client] add error provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import { AppProvider } from '@/providers/app';
 import PrivateRoute from '@/routers/privateRoute';
 import { privateRoutes, publicRoutes } from '@/routers';
 import { AuthProvider } from '@/context/AuthProvider';
+import { ErrorProvider } from './context/ErrorProvider';
+import ErrorNotification from './components/custom/error/errorNotification';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,23 +14,30 @@ function App() {
   return (
     <AppProvider>
       <AuthProvider>
-        <Routes>
-          {/* private routes */}
-          <Route element={<PrivateRoute />}>
-            {privateRoutes.map((route) => (
+        <ErrorProvider>
+          <ErrorNotification />
+          <Routes>
+            {/* private routes */}
+            <Route element={<PrivateRoute />}>
+              {privateRoutes.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={route.element}
+                />
+              ))}
+            </Route>
+
+            {/* public routes */}
+            {publicRoutes.map((route) => (
               <Route
                 key={route.path}
                 path={route.path}
                 element={route.element}
               />
             ))}
-          </Route>
-
-          {/* public routes */}
-          {publicRoutes.map((route) => (
-            <Route key={route.path} path={route.path} element={route.element} />
-          ))}
-        </Routes>
+          </Routes>
+        </ErrorProvider>
       </AuthProvider>
     </AppProvider>
   );
