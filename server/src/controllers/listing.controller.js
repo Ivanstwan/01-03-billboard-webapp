5a7c8e45fcce65f5af355d62fbe528735855674a
Ivanstwan <ivan.stwan@gmail.com>
2024-03-04 22:41:41 +0700

    [server] adjust add listing api

--- server/src/controllers/listing.controller.js
+++ server/src/controllers/listing.controller.js
@@ -2,48 +2,47 @@ import pool from '../config/db.config.js';
 
 // add listing
 const addListing = async (req, res) => {
+  const { user_id } = req.decoded;
+  const {
+    ads_name,
+    location,
+    ads_type_id,
+    latitude,
+    longitude,
+    size_height,
+    size_length,
+  } = req.body;
   try {
-    const { title, address, x, y, latitude, longitude, adsType } = req.body;
-
-    const latitudeValue = latitude !== '' ? latitude : null;
-    const longitudeValue = longitude !== '' ? longitude : null;
-
-    // Define a mapping object for adsType to ads_type_id
-    const adsTypeMapping = {
-      billboard: 1,
-      videotron: 2,
-      // Add more mappings as needed
-      // Option3: 3,
-      // Option4: 4,
-      // ... and so on
-    };
-
-    const adsTypeId = adsTypeMapping[adsType] || 99; // Default to 1 if not found in the mapping
-
     const query =
-      'INSERT INTO advertisement (ads_name, location, size_length, size_height, latitude, longitude, user_id, ads_type_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?);';
+      'INSERT INTO advertisement (ads_name, location, ads_type_id, latitude, longitude, size_height, size_length, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?);';
     const values = [
-      title,
-      address,
-      x,
-      y,
-      latitudeValue,
-      longitudeValue,
-      1,
-      adsTypeId,
+      ads_name,
+      location,
+      ads_type_id,
+      latitude,
+      longitude,
+      size_height,
+      size_length,
+      user_id,
     ];
 
     const [rows, fields] = await pool.query(query, values);
 
     if (rows.affectedRows === 1) {
       // If one row was affected (inserted), consider it a success
-      res.status(200).json({ success: true, message: 'Success Adding Data' });
+      res.status(200).json({
+        success: true,
+        // for redirect in client side
+        listing_id: rows.insertId,
+        message: 'Success Adding Data',
+      });
     } else {
       // Handle other cases, e.g., no rows affected
       res.status(400).json({ success: false, message: 'Insertion failed' });
     }
+    return;
   } catch (err) {
-    console.log('Error querying ads data:', err);
+    console.log('Error querying data:', err);
     res.status(500).json({ success: false, message: 'Internal Server Error' });
   }
 };
@@ -164,15 +163,15 @@ const editListing = async (req, res) => {
 
     if (rows.affectedRows === 1) {
       // If one row was affected (inserted), consider it a success
-      res.status(200).json('Listing Edit Successful.');
+      res.status(200).json({ success: true, message: 'Success Edit Listing' });
     } else {
       // Handle other cases, e.g., no rows affected
-      res.status(400).json('Failed to edit listing.');
+      res.status(400).json({ success: false, message: 'Edit failed' });
     }
     return;
   } catch (err) {
     console.log('Error querying data:', err);
-    res.status(500).send('Internal Server Error');
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
   }
 };
 
--- server/src/routers/listing.router.js
+++ server/src/routers/listing.router.js
@@ -26,13 +26,62 @@ router
     }
   );
 
-router
-  .route('/add')
-  .post(
-    body('title').notEmpty(),
-    body('address').notEmpty(),
-    listingController.addListing
-  );
+router.route('/add').post(
+  authenticateToken,
+  [
+    body('ads_name')
+      .notEmpty()
+      .withMessage('Advertisement Name cannot be empty'),
+    body('location').notEmpty().withMessage('Location cannot be empty'),
+    body('latitude')
+      .optional({ checkFalsy: true }) // Make it optional
+      .custom((value) => {
+        if (value && !_latitudeRegex.test(value)) {
+          throw new Error('Invalid latitude format');
+        }
+        return true;
+      }),
+    body('longitude')
+      .optional({ checkFalsy: true }) // Make it optional
+      .custom((value) => {
+        if (value && !_longitudeRegex.test(value)) {
+          throw new Error('Invalid longitude format');
+        }
+        return true;
+      }),
+    body('ads_type_id')
+      .notEmpty()
+      .withMessage('Advertisement Name cannot be empty')
+      .isNumeric()
+      .withMessage('Advertisement type format wrong.'),
+    body('size_height')
+      .optional({ checkFalsy: true }) // Make it optional
+      .custom((value) => {
+        if (value && !_sizeHeightRegex.test(value)) {
+          throw new Error('Invalid longitude format');
+        }
+        return true;
+      }),
+    body('size_length')
+      .optional({ checkFalsy: true }) // Make it optional
+      .custom((value) => {
+        if (value && !_sizeLengthRegex.test(value)) {
+          throw new Error('Invalid longitude format');
+        }
+        return true;
+      }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors, '[error]');
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    listingController.addListing(req, res);
+  }
+);
 
 router.route('/:id').get(listingController.getSingleListing);
 router.route('/edit/:id').post(
